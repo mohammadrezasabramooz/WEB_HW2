2a4599fe5aa2c76ad982dac08f48a5853fd72f40
AmirHossein <amiryousefi98@gmail.com>
2020-11-25 19:23:17 +0330

    script_v2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
+let nightModeClick = 0;
+
 function myFunction() {
-    const element = document.body;
+    nightModeClick += 1;
     const ele = document.getElementById("navbar");
-    const wrapper  = document.getElementById("sidebar-wrapper");
-    ele.classList.toggle("dark-mode");
-    wrapper.classList.toggle("dark-mode");
+    const wrapper = document.getElementById("sidebar-wrapper");
+    if (nightModeClick % 2 === 0) {
+        wrapper.style.backgroundColor = "#067cb5";
+        wrapper.style.color = "black";
+        ele.style.backgroundColor = "";
+        document.body.style.backgroundColor = "white";
+    }
+    if (nightModeClick % 2 === 1) {
+        wrapper.style.backgroundColor = "black";
+        wrapper.style.color = "white";
+        ele.style.backgroundColor = "black";
+        ele.style.color = "white";
+        document.body.style.backgroundColor = "black";
+    }
+
 }
 
+let clicked = 0;
+
+function showSideMenu() {
+    const sidebar = document.getElementById("sidebar-wrapper");
+    clicked = clicked + 1;
+    console.log(clicked);
+
+    if (clicked % 2 !== 0) {
+        console.log("HODKJENDK");
+        sidebar.style.display = "inline-flex";
+    }
+    if (clicked % 2 === 0) {
+        sidebar.style.display = "none";
+    }
+}
 
